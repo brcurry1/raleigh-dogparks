31476d277ee74f2c1666bcd502f9f675e23a9ff1
Brandon Curry <brcurry1@asu.edu>
2020-05-02 20:32:02 -0500

    Commit

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const request = require('request');
 const cloudinary = require('cloudinary');
-const CLOUDINARY_NAME1 = 'ddmeofkzuNew';
-const CLOUDINARY_KEY1 = '833162614866536New';
-const CLOUDINARY_SECRET1 = '7bChKulxDSPFbLgqSxIsSv3GcewNew';
+const CLOUDINARY_NAME1 = 'brcurry1';
+const CLOUDINARY_KEY1 = '492344841387744';
+const CLOUDINARY_SECRET1 = 'CanDJnw00VWlihIDtCM4rzh0esU';
 const router = express.Router();
 
 cloudinary.config({ 
